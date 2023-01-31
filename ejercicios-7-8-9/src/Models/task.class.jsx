621766f605ee81ecfd2919f8a8d57fa685bf00bd
galoss88 <gallogarcianicolas@gmail.com>
2023-01-31 17:01:30 -0300

    Update task.class.jsx

--- ejercicios-7-8-9/src/Models/task.class.jsx
+++ ejercicios-7-8-9/src/Models/task.class.jsx
@@ -1,3 +1,4 @@
+//Forma nueva ecma6, clases
 export default class Task {
   constructor(nombre, apellido, telefono, email, estadoConexion) {
     this.Nombre = nombre;
@@ -7,3 +8,11 @@ export default class Task {
     this.Estado = estadoConexion || false;
   }
 }
+ //forma antigua de clases
+// export default function Task (nombre, apellido, telefono, email, estadoConexion){
+//   this.Nombre = nombre;
+//   this.Apellido = apellido;
+//   this.Telefono = telefono;
+//   this.Email = email;
+//   this.Estado = estadoConexion || false;
+// }
